a9bfc4be9906939dbdc3de01d8d464ce8923b7c8
Abe <eaftahi@placer.ca.gov>
2025-08-18 14:58:27 -0700

    Update event and synched to PROD

--- Scripts/ASA;HAZVEG!HAZARDOUS VEGETATION!~!~.js
+++ Scripts/ASA;HAZVEG!HAZARDOUS VEGETATION!~!~.js
@@ -1,20 +1,35 @@
-/*======================================================================================/
-| Program : ASA;HazVeg!Hazardous Vegetation!~!~
-| Event   : ApplicationSubmitAfter
-|
-| Client  : Placer County, CA
-| Usage   : Application Submit After for all HazVeg records.
-| 
-|
-| Requires: EMSE 3.0 and Standard Choice: "EMSE_EXECUTE_OPTIONS": "SCRIPT" to be Active.
-|
-| Notes   : TDunn 10/15/2020 created script
-|
+/*======================================================================================/  
+| Program : ASA;HazVeg!Hazardous Vegetation!~!~  
+| Event   : ApplicationSubmitAfter  
+|  
+| Client  : Placer County, CA  
+| Usage   : Application Submit After for all HazVeg records.  
+|   
+|  
+| Requires: EMSE 3.0 and Standard Choice: "EMSE_EXECUTE_OPTIONS": "SCRIPT" to be Active.  
+|  
+| Notes   : TDunn 10/15/2020 created script  
+|         : Abe   03/31/2025 IT Request # 1986 - HazVeg Workflow Revise  
+|  
 /==========================================================================================*/
 
-if(currentUserID == "TDUNN") {
-	showDebug = true;
-}
+if (currentUserID == "EAFTAHI") { showDebug = true; }
 // Set received date in custom Fields
-logDebug("Should be updating Complaint date");
-editAppSpecific("Complaint / Request Received",dateAdd(null,0));
+logDebug("Executing EMSE ASA:HazVeg/Hazardous Vegetation/*/* ...");
+
+editAppSpecific("Complaint / Request Received", dateAdd(null, 0));
+
+/**   
+ *     
+ * Abe - 07/09/2024:  IT Request # 1986 - HazVeg Workflow Revise   
+ *
+*/
+
+//logDebug("******* parentCapId:" + parentCapId);
+if (publicUser)
+    editAppSpecific("Received via", "Online");
+
+if (!publicUser && !parentCapId ) 
+    sendAcknowledgementLtr2Applicant();
+
+//End of IT Request # 1986 - HazVeg Workflow Revise  
