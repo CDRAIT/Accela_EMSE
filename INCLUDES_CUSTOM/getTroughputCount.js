43a3b5b3d493494893202f91b5a93abb72c9c3a2
CDRAIT <127981913+CDRAIT@users.noreply.github.com>
2025-09-24 12:36:40 -0700

    Create getTroughputCount.js

---
+++ INCLUDES_CUSTOM/getTroughputCount.js
@@ -0,0 +1,20 @@
+function getTroughputCount(capIDString) {
+	// var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext").getOutput();
+	// var ds = initialContext.lookup("java:/AA");
+	// var conn = ds.getConnection(); 
+	var conn = aa.db.getConnection();
+	var result = "";
+	var B1_ALT_ID = "";
+	var getSQL = "select Count(B1_ACCESS_BY_ACA) as Test from B1PERMIT where B1_PER_SUB_TYPE = 'Throughput' AND B1_ALT_ID like ? AND B1_APPL_STATUS != 'Closed'";
+	var sSelect = conn.prepareStatement(getSQL);
+	sSelect.setString(1, capIDString);
+	var rs = sSelect.executeQuery();
+	while (rs.next()) {
+		result = rs.getString("Test");
+
+
+	}
+	rs.close();
+	conn.close();
+	return result;
+}
