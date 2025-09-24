5a460e9005aa8975ff61d1c1cb11d226d1628090
CDRAIT <127981913+CDRAIT@users.noreply.github.com>
2025-09-24 12:20:30 -0700

    Create copyOwnerTPS.js

---
+++ INCLUDES_CUSTOM/copyOwnerTPS.js
@@ -0,0 +1,16 @@
+function copyOwnerTPS(sCapID, tCapID)
+{
+	var ownrReq = aa.owner.getOwnerByCapId(sCapID);
+	if(ownrReq.getSuccess())
+	{
+		var ownrObj = ownrReq.getOutput();
+		for (xx in ownrObj)
+		{
+			ownrObj[xx].setCapID(tCapID);
+			aa.owner.createCapOwnerWithAPOAttribute(ownrObj[xx]);
+			logDebug("Copied Owner: " + ownrObj[xx].getOwnerFullName())
+		}
+	}
+	else
+		logDebug("Error Copying Owner : " + ownrObj.getErrorType() + " : " + ownrObj.getErrorMessage());
+}
