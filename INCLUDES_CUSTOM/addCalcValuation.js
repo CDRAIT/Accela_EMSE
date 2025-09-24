3f99aebcfddb1ee69902e7cdb786cbd8c9908112
CDRAIT <127981913+CDRAIT@users.noreply.github.com>
2025-09-24 12:32:30 -0700

    Create addCalcValuation.js

---
+++ INCLUDES_CUSTOM/addCalcValuation.js
@@ -0,0 +1,14 @@
+function addCalcValuation(occType, unitType, unitAmt, vVersion, vCapId) {
+	bVal = aa.finance.createBCalcValuatnScriptModel();
+	logDebug("unitType: " + unitType + "; occType: " + occType + "; unitAmt: " + unitAmt);
+	bVal.setAuditID("ADMIN");
+	bVal.setCapID(capId);
+	bVal.setConTyp(unitType);
+	bVal.setUnitValue(parseFloat(unitAmt));
+	bVal.setUseTyp(occType);
+	bVal.setVersion(vVersion);
+
+	r = aa.finance.createBCalcValuatn(bVal);
+
+	logDebug("Calculated Value is " + bVal.getTotalValue());
+}
