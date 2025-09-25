be77ffd02afa9c522255f064ceb4268e49a334a8
CDRAIT <127981913+CDRAIT@users.noreply.github.com>
2025-09-25 12:09:58 -0700

    Create setReviewWorkflowTasksByTsiFieldsPCO.js

---
+++ INCLUDES_CUSTOM/setReviewWorkflowTasksByTsiFieldsPCO.js
@@ -0,0 +1,43 @@
+function setReviewWorkflowTasksByTsiFieldsPCO(allTasksArray,allTriageArray) 
+{
+    // Only activate  review tasks from Triage list.
+    // This assumes all review tasks are parallel, and that the Workflow Task name is synonymous with the TSI field names.  i.e. Task Name = Building Review, 
+    // TSI Review = Building Review, TSI Review Date = Building Review Date
+    // Assumes TSI "Review Date" field has been set (by expression)
+
+    logDebug("Inside function setReviewWorkflowTasksByTsiFieldsPCO.  Params: " + wfProcess + "; task array: " + allTasksArray);
+
+    for (ata in allTasksArray) {
+        var taskRequired = false;
+        var thisTask = allTasksArray[ata];  //For each Review in list (all Review names are in List)
+		var statusIsNull = isTaskStatusNull(thisTask);
+        logDebug("thisTask = " + thisTask + " and AInfo[thisTask] = " + AInfo[thisTask]);
+		logDebug(thisTask + " status is " + getTaskStatus(thisTask) + ", is null = " + isTaskStatusNull(thisTask));
+        //If the Review TSI is set to Yes, set Required to True
+		for(tla in allTriageArray)
+		{
+			triageTask = allTriageArray[tla];
+			if (triageTask == thisTask && matches(AInfo[thisTask],"Yes","Y")) {
+				taskRequired = true;
+				logDebug("taskRequired was set to true");
+			}
+		}
+
+        if (taskRequired) {
+            logDebug("task is required so set Task Active");
+            activateTask(thisTask);
+            //editTaskDueDate(thisTask,AInfo[thisTask + " Due Date"]);	//Set the Task Due Date from the TSI Review Date field
+        }
+
+        if (!taskRequired && statusIsNull) 
+		{
+            logDebug("task not required and no history so setTask N and N");
+			setTask(thisTask,"N","N",wfProcess);
+        }
+        if (!taskRequired && !statusIsNull) 
+		{
+            logDebug("task not required but has history, setTask N and Y");
+            setTask(thisTask,"N","Y",wfProcess);
+		}
+    }
+}
