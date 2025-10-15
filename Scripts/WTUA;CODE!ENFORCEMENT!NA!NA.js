df62a6a0f150baade1470a8f6b7d11609c87b60e
Abe-Placer <eaftahi@placer.ca.gov>
2025-10-15 10:59:32 -0700

    New Code Enf WF Actions - V.1.0

--- Scripts/WTUA;CODE!ENFORCEMENT!NA!NA.js
+++ Scripts/WTUA;CODE!ENFORCEMENT!NA!NA.js
@@ -9,7 +9,7 @@
 | Requires: EMSE 3.0 and Standard Choice: "EMSE_EXECUTE_OPTIONS": "SCRIPT" to be Active.
 |
 | Notes   : eaftahi 11/22/2024 created script - Converted from StdChoice, WTUA:Code/~/~/~ branch
-|          
+|           EAFTAHI 10/01/2025 Added IT Request# 1675 - New Code Compliance WF
 |
 |
 /--------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -17,7 +17,124 @@
 if (currentUserID == "EAFTAHI") { showDebug = 1; }
 logDebug("In the WTUA:Code/Enforcement/*/* ...");
 
-if(matches(wfStatus,"Resolved","Notes", "Appeal Hearing CBO","Referred","Unfounded","No Violation")) {
-    removeParcelCondition(null,"Code","Unlawful Landuse"); 
-    removeParcelCondition(null,"Code Compliance - Notification","Unlawful Land Use");
+//Old WF
+if (matches(wfStatus, "Resolved", "Notes", "Appeal Hearing CBO", "Referred", "Unfounded", "No Violation")) {
+    removeParcelCondition(null, "Code", "Unlawful Landuse");
+    removeParcelCondition(null, "Code Compliance - Notification", "Unlawful Land Use");
 }
+
+
+
+if (wfProcess == "CODE_ENF") { //New Workflow
+    if (wfTask == "Complaint Recieved") {
+        if (wfStstus == "Unfounded") {
+            //Send Unfounded Letter to Applicant (2)
+        }
+        if (wfStstus == "Referred & Closed") {
+            //Send Referral Email to Agencies (4)
+            //Send Referral Letter to Applicant (3)
+        }
+        if (wfStatus == "Referred & Investigation") {
+            //Send Referral Email to Agencies (4)
+            //Send Referral Letter to Applicant (3)
+            // Send Courtesy Notice Letter to Parties (5)
+        }
+        if (wfStatus == "Courtesy Notice Sent") {
+            // Send Courtesy Notice Letter to Parties (5)
+        }
+    }
+
+    if (wfTask == "Investigation") {
+        if(wfStatus == "Referred & Closed") {
+            //send Referral Email to Agencies (4)
+            //Send Referral Letter to Applicant (3)
+        }
+        if(wfStatus == "Referred & Violation"){
+            //send Referral Email to Agencies (4)
+            //Send Referral Letter to Applicant (3)
+            
+        }
+    }
+
+    if (wfTask == "Enforcement Action") {
+        if (wfStatus == "NOV Mailed") {
+            //Create NOV Letter (6)
+        }
+        if (wfTask == "Citation & Notice of Nuisance") {
+            //Create Nuisance Letter (11)
+            //Nuisance Email to Staff (12)
+        }
+        if (wfTask == "Notice of Nuisance") {
+            closeTask("Enforcement Action", "Notice of Nuisance", "Updated by Script", null);
+            activateTask("Nuisance Outcome");
+            //Create Nuisance Letter (11)
+        }
+    }
+
+    if (wfTask == "Citation") {
+        if (wfStatus == "Citation") {
+            // Create Citation Letter (7)}
+            //Email Citation Appeal to Officer (8)
+        }
+    }
+
+    if (wfTask == "Appeal") {
+        if (wfStatus == "No Appeal") {
+            //Create the fork manually
+            closeTask("Appeal");
+            activateTask("Citation");
+            activateTask("Fine Processing");
+        }
+    }
+
+    if (wfTask == "Administrative Hearing") {
+        if (wfStatus == "Pending Hearing") {
+            //Send Hearing Letter (9)
+            //Send Complainant Hearing Notice (19)
+        }
+        if (wfStatus == "Continued") {
+            //Send Hearing Letter (9)
+            //Send Complainant Hearing Notice (19)
+        }
+        if (wfStatus == "Citation Upheld") {
+            //Email Citaion Upheld to Officer (10)            
+        }        
+    }
+
+    if (wfTask == "Abatement Hearing") {
+        if (matches(wfStatus, "Pending Hearing", "Continued")) {
+            //Nuicance Abatement Letter (13)
+        }
+        if (wfStatus == "Abatement Upheld") {
+            //Reinspection Email to staff (14) - or Create an inspection 
+        }
+    }
+
+    if (wfTask == "Reinspection Outcome") {
+        if (wfStatus == "In Violation - Enforcement") {
+            // Create the fork manually
+            closeTask("Reinspection Outcome");
+            activateTask("Enforcement Action");
+            activateTask("Abatement Processing");
+        }
+    }
+
+    if ((wfTask == "Citation" && wfStatus == "Complied") ||
+        (wfTask == "Abatement Processing" && wfStatus == "Abatement Complete") ||
+        (wfTask == "Administrative Hearing" && matches(wfStatus, "Complied", "Citation Upheld")) ||
+        (wfTask == "Appeal" && wfStatus == "No Appeal")) {
+        //Send Case Closed Letter (15)
+    }
+
+    if (wfTask == "Fine Processing") {
+        if (wfStatus == "Request for Payment") {
+            //Create Invoice Cover Letter (16)
+            //Eamil Fines Due to Staff (17)
+        }
+        if (wfStatus == "Subsequent Request") {
+            //Eamil Fines Due to Staff (17)
+            //Invoice Cover Letter, 2nd (18)
+        }
+    }
+}
+
