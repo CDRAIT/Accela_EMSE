ef1e03208d00d22be1c83acdc57bfcc9adffae1c
TerryDunn-TPS <38469696+hiker0696@users.noreply.github.com>
2025-11-17 09:19:35 -0800

    Update CTRCA;BUILDING!RESIDENTIAL!FULL REVIEW!~.js
    Changed Tech fee to assess as updateFee

--- Scripts/CTRCA;BUILDING!RESIDENTIAL!FULL REVIEW!~.js
+++ Scripts/CTRCA;BUILDING!RESIDENTIAL!FULL REVIEW!~.js
@@ -13,6 +13,7 @@
 |         : eaftahi 10/18/2023 modified the ADU/JADU section to cover the latest Custom Fields update
 |         : eaftahi 12/07/2023 added "Addressing" Ad Hoc task req# 1865
 |         : eaftahi 01/16/2025 IT Req# 2221 Fee deferral - SB937 
+|         : TDunn 11/17/2025 changed TECH fee to assess as updateFee instead of addFee
 |
 /==========================================================================================================*/
 
@@ -221,7 +222,7 @@ if(publicUser && !appMatch("Building/Residential/Limited/*")) {
 	}
 	 
 	if((appTypeArray[2] == "Full Review" && matches(appTypeArray[3],"Renewal","Other","Residential>3000","Residential<3000","Tract > 3000","Tract < 3000")) || matches(appTypeArray[2],"Plan Check Only","Limited")){
-		addFee("TECH","ACCOUNTING","FINAL",1,varAutoInvoiceFees);
+		updateFee("TECH","ACCOUNTING","FINAL",1,varAutoInvoiceFees);
 	}
 }
 
