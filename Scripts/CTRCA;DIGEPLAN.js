df51f01982cfb226fde579c690e7873eff686e55
mhelvick <66747217+mhelvick@users.noreply.github.com>
2025-12-31 14:22:57 -0800

    Create CTRCA;DIGEPLAN.js

---
+++ Scripts/CTRCA;DIGEPLAN.js
@@ -0,0 +1,36 @@
+//CTRCA:DIGEPLAN
+logDebug("<font color='green'>INSIDE CTRCA DIGEPLAN</font>");
+//create variable for tmpCapID in case the altID is updated earlier in the event.
+tmpCapId = aa.cap.getCapID(capId.getID1(),capId.getID2(),capId.getID3()).getOutput();
+if (tmpCapId) capId = tmpCapId;
+tmpCapIDString = capId.getCustomID();
+logDebug("tmpCapIDString : " + tmpCapIDString);
+
+try {
+	// Get the TMPRecordID custom field value
+	var tmpRecordID = getAppSpecific("TMPRecordID");
+	logDebug("TMPRecordID custom field: " + tmpRecordID);
+
+	if (tmpRecordID != undefined)  {
+		// Call the DigEplan TMP record conversion API
+		digEplanTmpRecordConversion(tmpCapIDString,tmpRecordID);
+	} else {
+		if (tmpRecordID == undefined) logDebug("<font color='red'>UNABLE TO CALL DIGEPLAN TMP RECORD CONVERSION API, AS TMPRecordID IS UNDEFINED</font>");
+	}
+}
+catch (err) {
+	logDebug("A JavaScript Error occured: " + err.message + " at line " + err.lineNumber + " stack: " + err.stack);
+}
+
+
+	//enter as many of these as there are customizations for doc groups by record type
+	docGroupForDPC = "";
+	if(AInfo["DocumentGroupforDPC"] == null) {
+		if(appMatch("Building/*/*/*")) docGroupForDPC = String("BLD_PLANREVIEW_DPC");
+		if(appMatch("Building/Deferred Submittal/*/*")) docGroupForDPC = String("DEFERRED");
+		if(appMatch("Building/Residential/PV Solar/*")) docGroupForDPC = String("BLD_SOLARAPP");
+
+		editAppSpecific("DocumentGroupforDPC",docGroupForDPC);
+		editAppSpecific("AdditionalDocumentTypes",selectDocConfigByGroupPermissions(docGroupForDPC,[]));
+	}
+	editAppSpecific("RequiredDocumentTypes","");
