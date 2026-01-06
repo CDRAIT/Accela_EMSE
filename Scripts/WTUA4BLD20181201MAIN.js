c46b141ff9af2e33f9c5f9b1be2a2ffc5c4e7505
TerryDunn-TPS <38469696+hiker0696@users.noreply.github.com>
2026-01-06 13:16:47 -0800

    Updates to script calls for old workflows
    Updates to script calls for old workflows

--- Scripts/WTUA4BLD20181201MAIN.js
+++ Scripts/WTUA4BLD20181201MAIN.js
@@ -58,96 +58,96 @@ if (wfProcess == 'BLD_20181201_MAIN' || wfProcess == 'BLD_20181201_DISTRIBUTION'
 	if (matches(wfTask, "Plan Check") && matches(wfStatus,"Complete", "Corrections Required", "Revision Prior to Issuance")) {
 		editTaskSpecific("Plan Check", "Plan Reviews", Number(AInfo['Plan Reviews']) + 1);
 		}
-}
-if (matches(wfTask,"Fire Review") && matches(wfStatus,"Complete","Revisions","Corrections Required") && AInfo['LPG Article 15.12'] == "CHECKED") 
-{
-	addStdCondition("Fire - Prevent Final / Completion", "Fire LPG Article 15.12 Inspection Required");
-}
 
-if (matches(wfTask,"Ready to Issue","Plan Check","Issue Status","Process for Issuance") && wfStatus == "Issued" && (getAppSpecific("Issue Date") == null || getAppSpecific("Issue Date") == "")) 
-{
-	editAppSpecific("Issue Date",dateAdd(null,0));
-}
-
-if (matches(wfTask,"Ready to Issue","Plan Check","Issue Status","Process for Issuance") && wfStatus == "Issued"){
-	editFirstIssuedDate(dateAdd(null,0));
-}
-
-if (matches(wfTask,"Ready to Issue","Plan Check","Issue Status","Process for Issuance") && wfStatus == "Issued" && cap.isCreatedByACA()) 
-{
-	addStdCondition("Building - Prevent Building Inspections","Applicant Signature & Final Department Processing Required");
-}
-
-// if (matches(wfTask,"Inspections") && matches(wfStatus,"Revisions")) 
-// {
-	// addStdCondition("Building - Prevent Final / Completion","Building Final Inspection Required");
-// }
-if(isTaskStatus("Department Distribution","Distribute","BLD_20181201_REVISIONS") || isTaskStatus("Department Distribution","Not Required - Approve Revisions","BLD_20181201_REVISIONS") || isTaskStatus("Department Distribution","Not Required - Plan Check Only","BLD_20181201_REVISIONS"))
-{	
-	if (matches(wfTask,"Revisions") && matches(wfStatus,"Revisions Approved")) 
+	if (matches(wfTask,"Fire Review") && matches(wfStatus,"Complete","Revisions","Corrections Required") && AInfo['LPG Article 15.12'] == "CHECKED") 
 	{
-		removeCapCondition("Building - Prevent Final / Completion","Building Final Inspection Required");
-	}
-}
-
-if (matches(wfTask,"Department Distribution") && matches(wfStatus,"Not Required - Approve Revisions")) 
-{
-	removeCapCondition("Building - Prevent Final / Completion","Building Final Not Allowed until Revisions are Approved");
-}
-
-if (matches(wfProcess, "BLD_20181201_REVISIONS") && matches(wfTask, "Department Distribution") && matches(wfStatus,"Not Required - Plan Check Only") && !(isTaskActive("Plan Check", "BLD_20181201_REVISIONS"))) {
-	activateTask("Plan Check", "BLD_20181201_REVISIONS");
-	deactivateTask("Department Distribution","BLD_20181201_REVISIONS");
+		addStdCondition("Fire - Prevent Final / Completion", "Fire LPG Article 15.12 Inspection Required");
 	}
 
-if (matches(wfTask,"Environmental Health Review") && matches(wfStatus,"Complete with PUD Conditions"))
-{
-	addStdCondition("PUD - Prevent Final / Completion","Water");
-	addStdCondition("PUD - Prevent Final / Completion","Sewer");
-}
-
-if (matches(wfTask,"Plan Check") && wfStatus == "Complete" && wfProcess == "BLD_20181201_MAIN") {
-	var AInfoTSItemp = [];
-	useTaskSpecificGroupName = true;
-	loadTaskSpecific(AInfoTSItemp);
-	editAppSpecific("Plan Check Type", AInfoTSItemp["BLD_20181201_MAIN.Plan Check.Plan Check Type Override"]);
-	useTaskSpecificGroupName = false;
+	if (matches(wfTask,"Ready to Issue","Plan Check","Issue Status","Process for Issuance") && wfStatus == "Issued" && (getAppSpecific("Issue Date") == null || getAppSpecific("Issue Date") == "")) 
+	{
+		editAppSpecific("Issue Date",dateAdd(null,0));
 	}
 
-if (matches(wfTask,"Plan Check") && wfStatus == "Complete" && wfProcess == "BLD_20181201_REVISIONS")
-{
-	var AInfoTSItemp = [];
-	useTaskSpecificGroupName = true;
-	loadTaskSpecific(AInfoTSItemp);
-	editAppSpecific("Plan Check Type", AInfoTSItemp["BLD_20181201_REVISIONS.Plan Check.Plan Check Type Override"]);
-	useTaskSpecificGroupName = false;
+	if (matches(wfTask,"Ready to Issue","Plan Check","Issue Status","Process for Issuance") && wfStatus == "Issued"){
+		editFirstIssuedDate(dateAdd(null,0));
+	}
+
+	if (matches(wfTask,"Ready to Issue","Plan Check","Issue Status","Process for Issuance") && wfStatus == "Issued" && cap.isCreatedByACA()) 
+	{
+		addStdCondition("Building - Prevent Building Inspections","Applicant Signature & Final Department Processing Required");
+	}
+
+	// if (matches(wfTask,"Inspections") && matches(wfStatus,"Revisions")) 
+	// {
+		// addStdCondition("Building - Prevent Final / Completion","Building Final Inspection Required");
+	// }
+	if(isTaskStatus("Department Distribution","Distribute","BLD_20181201_REVISIONS") || isTaskStatus("Department Distribution","Not Required - Approve Revisions","BLD_20181201_REVISIONS") || isTaskStatus("Department Distribution","Not Required - Plan Check Only","BLD_20181201_REVISIONS"))
+	{	
+		if (matches(wfTask,"Revisions") && matches(wfStatus,"Revisions Approved")) 
+		{
+			removeCapCondition("Building - Prevent Final / Completion","Building Final Inspection Required");
+		}
+	}
+
+	if (matches(wfTask,"Department Distribution") && matches(wfStatus,"Not Required - Approve Revisions")) 
+	{
+		removeCapCondition("Building - Prevent Final / Completion","Building Final Not Allowed until Revisions are Approved");
+	}
+
+	if (matches(wfProcess, "BLD_20181201_REVISIONS") && matches(wfTask, "Department Distribution") && matches(wfStatus,"Not Required - Plan Check Only") && !(isTaskActive("Plan Check", "BLD_20181201_REVISIONS"))) {
+		activateTask("Plan Check", "BLD_20181201_REVISIONS");
+		deactivateTask("Department Distribution","BLD_20181201_REVISIONS");
+		}
+
+	if (matches(wfTask,"Environmental Health Review") && matches(wfStatus,"Complete with PUD Conditions"))
+	{
+		addStdCondition("PUD - Prevent Final / Completion","Water");
+		addStdCondition("PUD - Prevent Final / Completion","Sewer");
+	}
+
+	if (matches(wfTask,"Plan Check") && wfStatus == "Complete" && wfProcess == "BLD_20181201_MAIN") {
+		var AInfoTSItemp = [];
+		useTaskSpecificGroupName = true;
+		loadTaskSpecific(AInfoTSItemp);
+		editAppSpecific("Plan Check Type", AInfoTSItemp["BLD_20181201_MAIN.Plan Check.Plan Check Type Override"]);
+		useTaskSpecificGroupName = false;
+		}
+
+	if (matches(wfTask,"Plan Check") && wfStatus == "Complete" && wfProcess == "BLD_20181201_REVISIONS")
+	{
+		var AInfoTSItemp = [];
+		useTaskSpecificGroupName = true;
+		loadTaskSpecific(AInfoTSItemp);
+		editAppSpecific("Plan Check Type", AInfoTSItemp["BLD_20181201_REVISIONS.Plan Check.Plan Check Type Override"]);
+		useTaskSpecificGroupName = false;
+	}
+
+	if (wfProcess == "BLD_20181201_DISTRIBUTION" && wfTask == "TRPA ESD Review" && wfStatus == "Complete" && isTaskComplete("TRPA Planning Review","BLD_20181201_DISTRIBUTION")) 
+	{
+		assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_DISTRIBUTION");
+	}
+
+	if (wfProcess == "BLD_20181201_REVISIONS" && wfTask == "TRPA ESD Review" && wfStatus == "Complete" && isTaskComplete("TRPA Planning Review","BLD_20181201_REVISIONS")) 
+	{
+		assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_REVISIONS");
+	}
+
+	if (wfProcess == "BLD_20181201_DISTRIBUTION" && wfTask == "TRPA Planning Review" && wfStatus == "Complete" && isTaskComplete("TRPA ESD Review","BLD_20181201_DISTRIBUTION")) 
+	{
+		assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_DISTRIBUTION");
+	}
+
+	if (wfProcess == "BLD_20181201_REVISIONS" && wfTask == "TRPA Planning Review" && wfStatus == "Complete" && isTaskComplete("TRPA ESD Review","BLD_20181201_REVISIONS"))
+	{
+		assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_REVISIONS");
+	}
+
+	if (wfProcess == "BLD_20181201_DISTRIBUTION" && wfTask == "Planning Review" && (wfStatus == "Complete" || wfStatus == "Corrections Required" ) && AInfo["PCCP Required"] == "Yes") 
+	{
+		addAdHocTask("ADHOC", "Planning Review", "PCCP");
+		addAppCondition("Planning - Prevent Issuance / Approval", "Applied", "PCCP Review Required", "Added by automation for PCCP Required Review", "Notice");
+	}
+
 }
-
-if (wfProcess == "BLD_20181201_DISTRIBUTION" && wfTask == "TRPA ESD Review" && wfStatus == "Complete" && isTaskComplete("TRPA Planning Review","BLD_20181201_DISTRIBUTION")) 
-{
-	assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_DISTRIBUTION");
-}
-
-if (wfProcess == "BLD_20181201_REVISIONS" && wfTask == "TRPA ESD Review" && wfStatus == "Complete" && isTaskComplete("TRPA Planning Review","BLD_20181201_REVISIONS")) 
-{
-	assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_REVISIONS");
-}
-
-if (wfProcess == "BLD_20181201_DISTRIBUTION" && wfTask == "TRPA Planning Review" && wfStatus == "Complete" && isTaskComplete("TRPA ESD Review","BLD_20181201_DISTRIBUTION")) 
-{
-	assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_DISTRIBUTION");
-}
-
-if (wfProcess == "BLD_20181201_REVISIONS" && wfTask == "TRPA Planning Review" && wfStatus == "Complete" && isTaskComplete("TRPA ESD Review","BLD_20181201_REVISIONS"))
-{
-	assignTask("TRPA Review", "TAHOE_COUNTER", "BLD_20181201_REVISIONS");
-}
-
-if (wfProcess == "BLD_20181201_DISTRIBUTION" && wfTask == "Planning Review" && (wfStatus == "Complete" || wfStatus == "Corrections Required" ) && AInfo["PCCP Required"] == "Yes") 
-{
-	addAdHocTask("ADHOC", "Planning Review", "PCCP");
-	addAppCondition("Planning - Prevent Issuance / Approval", "Applied", "PCCP Review Required", "Added by automation for PCCP Required Review", "Notice");
-}
-
-
-!matches(wfProcess,'BLD_20181201_MAIN','BLD_20181201_DISTRIBUTION','BLD_20181201_REVISIONS',"BLD_MAIN_20230501_MAIN","BLD_20231116_REV","BLD_DEFERRED_20240710","BLD_20241008_PC0") ^ branch("WTUA_wfProcess-Legacy_BLD");
+//!matches(wfProcess,'BLD_20181201_MAIN','BLD_20181201_DISTRIBUTION','BLD_20181201_REVISIONS',"BLD_MAIN_20230501_MAIN","BLD_20231116_REV","BLD_DEFERRED_20240710","BLD_20241008_PC0") ^ branch("WTUA_wfProcess-Legacy_BLD");
