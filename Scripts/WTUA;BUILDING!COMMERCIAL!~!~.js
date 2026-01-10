bb7af14f659293db3479d42a4d0a997cefe9424a
TerryDunn-TPS <38469696+hiker0696@users.noreply.github.com>
2026-01-10 11:55:08 -0800

    Update WTUA;BUILDING!COMMERCIAL!~!~.js

--- Scripts/WTUA;BUILDING!COMMERCIAL!~!~.js
+++ Scripts/WTUA;BUILDING!COMMERCIAL!~!~.js
@@ -1,4 +1,4 @@
-/*------------------------------------------------------------------------------------------------------/
+/*-------------------------------------------------------------------------------------------------------------------/
 | Program : WTUA;Building!Commercial!~!~
 | Event   : WorkflowTaskUpdateAfter
 |
@@ -19,8 +19,9 @@
 |         : TDunn 03/05/2024 added BLD_20181201_MAIN criteria to rules for issuance
 |         : Abe   06/27/2024 added IT Request # 1924 - ESD Building Permit Sign Off - "ESD Checklist"
 |         : TDunn 12/30/2025 updated criteria for setting Plan Check Expiration date
+|         : TDunn 01/10/2026 Moved Revisions and Deferred Submittal from Parent by staff from WTUA:Building to here
 |
-/------------------------------------------------------------------------------------------------------*/
+/-----------------------------------------------------------------------------------------------------------------------*/
 if (matches(currentUserID, "TDUNN", "EAFTAHI", "MHELVIC")) {
 	showDebug = 1;
 }
@@ -208,4 +209,199 @@ if (appTypeArray[2] == "Limited")
 	}
   }
 }
-//End of IT Request# 1911 - EV Charging Station  
+//End of IT Request# 1911 - EV Charging Station 
+
+if(matches(wfProcess,"BLD_20181201_DISTRIBUTION","BLD_20181201_MAIN","BLD_20230501_MAIN"))
+{
+	if(matches(wfTask,"Inspections","Inspection"))
+	{
+		if(wfStatus == "Revisions")
+		{
+			logDebug("Inside creating revision child record");
+			var recName = "Building Permit Revision for " + capIDString;
+			var cCapId = createChild("Building","Revision","NA","NA",recName); 
+			var pCapId = capId;
+			var newAltID = "";
+			var childExt = "-REV";
+			var NewSn = getShortNotes(pCapId);
+			var pWorkDesc = workDescGet(pCapId);
+			// Initialize Last Rev number if null
+			if(matches(AInfo["Last Revision Number"],null,"")) 
+			{
+				editAppSpecific("Last Revision Number",0);
+				AInfo["Last Revision Number"] = 0;
+			}
+			var revNumber = 1 * AInfo["Last Revision Number"];
+
+			logDebug("Child Type is :"+ childExt);
+			revNumber = revNumber + 1;
+			logDebug("Rev Number is " + revNumber);
+			editAppSpecific("Last Revision Number",revNumber);
+			var parentID = capIDString;
+			logDebug("Current Record Number is " + parentID);
+			// newAltID = capIDString + childExt + String(revNumber);
+			newAltID = capIDString + childExt + formatRevNumber(revNumber);
+			
+			aa.cap.updateCapAltID(cCapId, newAltID);	
+			logDebug("Child AltID = " + newAltID);
+			
+			copyOwnerTPS(pCapId,cCapId);
+			var assignedTo = getAssignedToStaff(pCapId); 
+			if(assignedTo != null && assignedTo != "") {
+				assignCap(assignedTo,cCapId);
+			}
+			copyAddresses(pCapId,cCapId);
+			copyParcels(pCapId,cCapId);
+			updateAppStatus("Issued - Revision Pending", "Revision " + formatRevNumber(revNumber) + " created by staff. Updated by Script", capId)
+
+			editAppSpecific("Project Office",getAppSpecific("Project Office",pCapId),cCapId);
+			editAppSpecific("Type of Work",getAppSpecific("Type of Work",pCapId),cCapId);
+			editAppSpecific("Scope of Work",getAppSpecific("Scope of Work",pCapId),cCapId);	
+			editAppSpecific("Plan Check Type",getAppSpecific("Plan Check Type",pCapId),cCapId);
+			copyContacts(pCapId,cCapId);
+
+			// Create notification to applicant for new Revision record created
+			var vEmailTemplate = "ONLINE_PERMIT_AMENDMENT_SUBMITTED";
+			var vEmailSent = false;
+			var vFromEmail = "";
+			var vToEmail = "";
+			var vCcEmail = "";
+			var pCapIDString = capIDString;
+			var emailParameters = aa.util.newHashtable();
+
+			// Load parameters for notification
+			addParameter(emailParameters,"$$parentAltId$$",pCapIDString);
+			addParameter(emailParameters,"$$childaltID$$",newAltID);
+			addParameter(emailParameters,"$$recNameParam$$",recName);
+			addParameter(emailParameters,"$$amendType$$","Revision");
+			addParameter(emailParameters,"$$projectoffice$$", getAppSpecific("Project Office", pCapId));
+			addParameter(emailParameters,"$$scopeOfWork$$",getAppSpecific("Scope of Work",pCapId));
+
+			// Parameters returned by getRecordParameters4Notification: $$altID$$; $$capName$$; $$capStatus$$; $$fileDate$$; $$workDesc$$; $$balanceDue$$; $$capTypeAlias$$
+			getRecordParams4Notification(emailParameters); 
+			getPrimaryAddressLineParam4Notification(emailParameters); /* returns $$addressLine$$ parameter */
+
+			/* Get To email contact types */
+			var cTypeArray = ["Applicant","Owner"];
+
+			/* Get To emails for contacts */
+			var conArray = new Array();
+			conArray = getContactArrayWithPrimary(capId); 
+			for (thisCon in conArray) {
+				if (exists(conArray[thisCon]["contactType"],cTypeArray)) {
+					logDebug(conArray[thisCon]["contactType"]) ;
+					getContactParams4Notification(emailParameters, conArray[thisCon]);
+					if(!matches(emailParameters.get("$$contactEmail$$"),"",null,undefined,false))
+					{
+						vToEmail = vToEmail + emailParameters.get("$$contactEmail$$") + "; ";
+					}
+				}
+			}
+			logDebug("vFromEmail= " + vFromEmail + "; vToEmail= " + vToEmail + "; vCcEmail = " + vCcEmail + "; emailTemplate= " + vEmailTemplate + "; emailParameters= " + emailParameters);
+			vEmailSent = sendNotification(vFromEmail,vToEmail,vCcEmail,vEmailTemplate,emailParameters, null);
+			logDebug("Email was sent: " + vEmailSent);
+			
+			showMessage = true;
+			comment("<font size = 4 color=ff000><b>Revision record created. Record number " + newAltID + ".</b></font><br><br>You can navigate to the new record using the Related Records tab.<br>");
+
+			// Existing rules for adding conditions on Revisions at Inspection task, modified to use new revision condition
+			//--------------------------------------------------------------------
+			if(!appHasCondition("Building - Prevent Final / Completion", "Applied","Building Final Not Allowed until Revisions are Approved", null))
+			{
+				addStdCondition("Building - Prevent Final / Completion","Building Final Not Allowed until Revisions are Approved");
+			}
+		}
+		// Inspections/Deferred Submittal: Create Deferred from parent record by Staff
+		if(wfStatus == "Deferred Submittal")
+		{
+			logDebug("Inside creating deferred submittal child record");
+			var recName = "Building Permit Deferred Submittal for " + capIDString;
+			var cCapId = createChild("Building","Deferred Submittal","NA","NA",recName); 
+			var pCapId = capId;
+			var newAltID = "";
+			var childExt = "-DEF";
+			var NewSn = getShortNotes(pCapId);
+			var pWorkDesc = workDescGet(pCapId);
+			// Initialize Last DEF number if null
+			if(matches(AInfo["Deferred Submittal Number"],null,"")) 
+			{
+				editAppSpecific("Deferred Submittal Number",0);
+				AInfo["Deferred Submittal Number"] = 0;
+			}
+			var defNumber = 1 * AInfo["Deferred Submittal Number"];
+
+			logDebug("Child Type is :"+ childExt);
+			defNumber = defNumber + 1;
+			logDebug("DEF Number is " + defNumber);
+			editAppSpecific("Deferred Submittal Number",defNumber);
+			var parentID = capIDString;
+			logDebug("Current Record Number is " + parentID);
+			newAltID = capIDString + childExt + formatRevNumber(defNumber);
+			
+			aa.cap.updateCapAltID(cCapId, newAltID);	
+			logDebug("Child AltID = " + newAltID);
+			
+			copyOwnerTPS(pCapId,cCapId);
+			var assignedTo = getAssignedToStaff(pCapId); 
+			if(assignedTo != null && assignedTo != "") {
+				assignCap(assignedTo,cCapId);
+			}
+			copyAddresses(pCapId,cCapId);
+			copyParcels(pCapId,cCapId);	
+			editAppSpecific("Project Office",getAppSpecific("Project Office",pCapId),cCapId);
+			editAppSpecific("Type of Work",getAppSpecific("Type of Work",pCapId),cCapId);
+			editAppSpecific("Scope of Work",getAppSpecific("Scope of Work",pCapId),cCapId);	
+			editAppSpecific("Plan Check Type",getAppSpecific("Plan Check Type",pCapId),cCapId);
+			
+			// Generate email notice to parent applicant for new Deferred Submittal application createDocumentFragment
+			var vEmailTemplate = "ONLINE_PERMIT_AMENDMENT_SUBMITTED";
+			var pCapIDString = capIDString;
+			var vEmailSent = false;
+			var vFromEmail = "";
+			var vToEmail = "";
+			var vCcEmail = "";
+			var emailParameters = aa.util.newHashtable();
+
+			addParameter(emailParameters,"$$parentAltId$$",pCapIDString);
+			addParameter(emailParameters,"$$childaltID$$",newAltID);
+			addParameter(emailParameters,"$$recNameParam$$",recName);
+			addParameter(emailParameters,"$$amendType$$","Deferred Submittal");
+			addParameter(emailParameters,"$$projectoffice$$", getAppSpecific("Project Office", pCapId));
+			addParameter(emailParameters,"$$scopeOfWork$$",getAppSpecific("Scope of Work",pCapId));
+			
+			// Parameters returned by getRecordParameters4Notification: $$altID$$; $$capName$$; $$capStatus$$; $$fileDate$$; $$workDesc$$; $$balanceDue$$; $$capTypeAlias$$
+			getRecordParams4Notification(emailParameters); 
+			getPrimaryAddressLineParam4Notification(emailParameters); /* returns $$addressLine$$ parameter */	
+			
+			/* Get To email contact types */
+			var cTypeArray = ["Applicant"];
+
+			/* Get To emails for contacts */
+			var conArray = new Array();
+			conArray = getContactArrayWithPrimary(capId); 
+			for (thisCon in conArray) 
+			{
+				if (exists(conArray[thisCon]["contactType"],cTypeArray)) 
+				{
+					logDebug(conArray[thisCon]["contactType"]) ;
+					getContactParams4Notification(emailParameters, conArray[thisCon]);
+					if(emailParameters.get("$$contactEmail$$") != null) 
+					{
+						vToEmail = vToEmail + emailParameters.get("$$contactEmail$$") + "; ";
+					}
+				}
+			}
+			logDebug("vFromEmail= " + vFromEmail + "; vToEmail= " + vToEmail + "; vCcEmail = " + vCcEmail + "; emailTemplate= " + vEmailTemplate + "; emailParameters= " + emailParameters);
+			vEmailSent = sendNotification(vFromEmail,vToEmail,vCcEmail,vEmailTemplate,emailParameters, null);			
+			
+			showMessage = true;
+			comment("<font size = 4 color=ff000><b>Deferred Submittal record created. Record number " + newAltID + ".</b></font><br><br>You can navigate to the new record using the Related Records tab.<br>");
+			
+			// New rules for adding conditions on deferred submittal
+			if(!appHasCondition("Building - Prevent Final / Completion","Applied","Building Final Not Allowed until Deferred Submittals are Approved",null))
+			{
+				addStdCondition("Building - Prevent Final / Completion","Building Final Not Allowed until Deferred Submittals are Approved");
+			}
+		}		
+	}
+}
