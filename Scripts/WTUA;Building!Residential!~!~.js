29a9e1e0228c77328e0929e058e1694761003170
TerryDunn-TPS <38469696+hiker0696@users.noreply.github.com>
2026-01-22 16:33:21 -0800

    Added Parent record type

--- Scripts/WTUA;Building!Residential!~!~.js
+++ Scripts/WTUA;Building!Residential!~!~.js
@@ -115,6 +115,7 @@ if(matches(wfProcess,"BLD_20181201_DISTRIBUTION","BLD_20181201_MAIN","BLD_202305
 			editAppSpecific("Type of Work",getAppSpecific("Type of Work",pCapId),cCapId);
 			editAppSpecific("Scope of Work",getAppSpecific("Scope of Work",pCapId),cCapId);	
 			editAppSpecific("Plan Check Type",getAppSpecific("Plan Check Type",pCapId),cCapId);
+			editAppSpecific("Parent Record Type",appTypeArray[1]);
 			copyContacts(pCapId,cCapId);
 
 			// Create notification to applicant for new Revision record created
@@ -209,6 +210,7 @@ if(matches(wfProcess,"BLD_20181201_DISTRIBUTION","BLD_20181201_MAIN","BLD_202305
 			editAppSpecific("Type of Work",getAppSpecific("Type of Work",pCapId),cCapId);
 			editAppSpecific("Scope of Work",getAppSpecific("Scope of Work",pCapId),cCapId);	
 			editAppSpecific("Plan Check Type",getAppSpecific("Plan Check Type",pCapId),cCapId);
+			editAppSpecific("Parent Record Type",appTypeArray[1]);
 			
 			// Generate email notice to parent applicant for new Deferred Submittal application createDocumentFragment
 			var vEmailTemplate = "ONLINE_PERMIT_AMENDMENT_SUBMITTED";
