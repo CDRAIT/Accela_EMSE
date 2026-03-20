4e22243925fcd1e5445d0de488850b9568aa6de3
TerryDunn-TPS <38469696+hiker0696@users.noreply.github.com>
2026-03-20 08:23:18 -0700

    Update WTUB;Building!~!~!~.js
    Moved custom function from this script to Includes_custom; remarked out Revisions at Inspection rule, replaced by expression

--- Scripts/WTUB;Building!~!~!~.js
+++ Scripts/WTUB;Building!~!~!~.js
@@ -20,6 +20,8 @@
 |         : TDunn   02/24/2025 added new status for completed Revision: 'Approved' for active Preissue task rule
 |         : TDunn   03/19/2026 updated Permit issuance rules for fees due to Signature Requested
 |         : TDunn   03/20/2026 deployed to nonprod1 for testing
+|         : TDunn   03/20/2026 removed CreateCollectionOfParcels function from script and moved to Includes_Custom
+|         : TDunn   03/20/2026 remarked out Revisions at Inspection task, replaced by expression
 |
 /=================================================================================================*/
 if(matches(currentUserID,"TDUNN","JMCKENZI","EAFTAHI")) { showDebug = 1;}
@@ -310,73 +312,17 @@ if(wfProcess == "BLD_20230501_MAIN" || wfProcess == "BLD_20231116_REV")
 		}		
 	}
 	// Process Rules for Inspections task
-	if(matches(wfTask,"Inspections","Inspection"))
-	{
-		if(wfStatus == "Revisions")
-		{
-			if(capStatus == "Issued - Revision Pending")
-			{
-				showMessage = true;
-				comment("<font size = 4 color=ff000><b>Permit status is " + capStatus + "!.</b></font><br><br>Only one revision can be in process at a time. An additional revision cannot be created until the current one has been completed");
-				cancel = true;
-			}				
-		}
-	}
+	// if(matches(wfTask,"Inspections","Inspection"))
+	// {
+		// if(wfStatus == "Revisions")
+		// {
+			// if(capStatus == "Issued - Revision Pending")
+			// {
+				// showMessage = true;
+				// comment("<font size = 4 color=ff000><b>Permit status is " + capStatus + "!.</b></font><br><br>Only one revision can be in process at a time. An additional revision cannot be created until the current one has been completed");
+				// cancel = true;
+			// }				
+		// }
+	// }
 }
 
-//-----------------------------------------------------------------------------
-// Internal functions to be moved to be moved to Includes_Custom on deployment
-//=============================================================================
-
-function createCollectionOfParcels()
-{
-	var capParcelResult = aa.parcel.getParcelandAttribute(capId,null);
-	varParcels = capParcelResult.getOutput().toArray();
-	if (varParcels != null) 
-	{
-		for (varEachParcel in varParcels)
-		{
-			// converted from branch CreateCollectionOfParcelConditions
-			varParcelNumber = varParcels[varEachParcel].getParcelNumber();
-			varParcelConditions = aa.parcelCondition.getParcelConditions(varParcelNumber);
-			varConditions = varParcelConditions.getOutput();
-			if (varConditions != null) 
-			{
-				for (varEachCondition in varConditions)
-				{
-					// converted from branch ValidateParcelConditions
-					varCondTypeCode = varConditions[varEachCondition].getConditionType();
-					varCondStatus = varConditions[varEachCondition].getConditionStatus();
-					varCondDesc = varConditions[varEachCondition].getConditionDescription();
-					if (varValidateOn == "Issue" && varCondTypeCode == "Parcel" && varCondDesc == "Hopkins Village Affordable Housing" && varCondStatus == "Applied") 
-					{
-						showMessage = true;
-						comment("<font size = 4 color=ff000>This permit cannot be issued because the <b>Hopkins Village Affordable Housing Condition</b> has not been met</font>");
-						cancel = true;
-					}
-
-					if (varValidateOn == "Issue" && varCondStatus == "Applied" && (varCondTypeCode == "Building - Prevent Issuance / Approval" || varCondTypeCode == "Planning - Prevent Issuance / Approval" || varCondTypeCode == "ESD - Prevent Issuance / Approval" || varCondTypeCode == "Env. Engineering - Prevent Issuance / Approval" || varCondTypeCode == "Code Compliance - Prevent Issuance / Approval" || varCondTypeCode == "Env. Health - Prevent Issuance / Approval" || varCondTypeCode == "DPW - Prevent Issuance / Approval" || varCondTypeCode == "Fire - Prevent Issuance / Approval" || varCondTypeCode == "Other - Prevent Issuance / Approval")) 
-					{
-						showMessage = true;
-						comment("<font size = 4 color=ff000><b>There are applied Parcel Conditions that must be cleared before proceeding.</b></font><br><br>");
-						cancel = true;
-					}
-
-					if (varValidateOn == "Final" && varCondTypeCode == "Parcel" && varCondDesc == "FEE DEFERRAL" && varCondStatus == "Applied") 
-					{
-						showMessage = true;
-						comment("<font size = 4 color=ff000>This permit cannot be finaled because the <b>Fee Deferral Condition</b> has not been met.</font>");
-						cancel = true;
-					}
-
-					if (varValidateOn == "Final" && varCondStatus == "Applied" && (varCondTypeCode == "Building - Prevent Final / Completion" || varCondTypeCode == "Planning - Prevent Final / Completion" || varCondTypeCode == "ESD - Prevent Final / Completion" || varCondTypeCode == "Env. Engineering - Prevent Final / Completion" || varCondTypeCode == "Code Compliance - Prevent Final / Completion" || varCondTypeCode == "Env. Health - Prevent Final / Completion" || varCondTypeCode == "DPW - Prevent Final / Completion" || varCondTypeCode == "Fire - Prevent Final / Completion" || varCondTypeCode == "Other - Prevent Final / Completion")) 
-					{
-						showMessage = true;
-						comment("<font size = 4 color=ff000>This permit cannot be finaled because the all Parcel Conditions have not been met.</font>");
-						cancel = true;
-					}
-				}
-			}
-		}
-	}
-}
