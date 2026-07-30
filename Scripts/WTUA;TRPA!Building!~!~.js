b21d20fb95bcab647e67509dedc35cef18c6ce59
TerryDunn-TPS <38469696+hiker0696@users.noreply.github.com>
2026-07-30 09:32:44 -0700

    Update WTUA;TRPA!Building!~!~.js

--- Scripts/WTUA;TRPA!Building!~!~.js
+++ Scripts/WTUA;TRPA!Building!~!~.js
@@ -164,9 +164,11 @@ if(matches(wfProcess,"BLD_20181201_DISTRIBUTION","BLD_20181201_MAIN"))
 			copyOwnerTPS(pCapId,cCapId);
 
 			// Auto assign and set due date for Submittal Review
-			capId = cCapId;			
-			assignTask("Submittal Review","CDRA_UNASSIGNED","BLD_DEFERRED_20240710");
+			capId = cCapId;
 			editTaskDueDate("Submittal Review",dateAdd(null,2,"Y"),"BLD_DEFERRED_20240710");
+			assignTask("Submittal Review","PERMIT CENTER_UNASSIGNED","BLD_DEFERRED_20240710");
+			editTaskSpecific("Submittal Review","Possession Start Date",dateAdd(null,0,"Y"),cCapId);
+			updateTask("Submittal Review","Received","Possession Start Date logged by system","","BLD_DEFERRED_20240710",cCapId);			
 			capId = pCapId;
 			copyAddresses(pCapId,cCapId);
 			copyParcels(pCapId,cCapId);	
