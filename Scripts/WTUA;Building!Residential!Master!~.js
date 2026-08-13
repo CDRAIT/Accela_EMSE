277a769cb4ae4be9a8875d35b480eea95a4afff2
TerryDunn-TPS <38469696+hiker0696@users.noreply.github.com>
2026-08-13 13:47:42 -0700

    Update WTUA;Building!Residential!Master!~.js

--- Scripts/WTUA;Building!Residential!Master!~.js
+++ Scripts/WTUA;Building!Residential!Master!~.js
@@ -24,6 +24,7 @@
 |         : TDunn 03/20/2026 reenabled update to Expiration Date and updated period to 120 months for dateAddMonths
 |         : TDunn 03/20/2026 deployed to production
 |         : TDunn 07/15/2026 added in possession date rules
+|         : TDunn 08/13/2026 added in possession date rules for Submittal Review for Revisions and Deferred creation
 |
 /--------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
